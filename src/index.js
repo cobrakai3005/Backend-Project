0f4f5700f4771f836708e16c487073307446a3c5
Himanshu Dhammeriya <cobrakai3005@gmail.com>
2024-04-09 10:42:29 +0530

    Utility Code : Api Responses Class and ApiError Class

--- src/index.js
+++ src/index.js
@@ -5,25 +5,4 @@ dotenv.config({path: "./env"})
 connectDB()
 
 
-/*
-import express from 'express';
-const app = express()
 
-;(async()=>{
-    try {
-    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-    app.on('error', (error)=>{
-        console.log("ERROR: ", error);
-        throw error
-    })
-
-    app.listen(process.env.PORT, ()=>{
-        console.log(`App is listening on PORT ${process.env.PORT}`);
-    })
-    } catch (error) {
-        console.log("ERROR: ", error);
-        throw error
-    }
-})()
-
-*/
