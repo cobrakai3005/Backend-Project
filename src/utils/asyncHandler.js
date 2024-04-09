00d0ebd743b15bf12c6062d9dc38831ba56b985b
Himanshu Dhammeriya <cobrakai3005@gmail.com>
2024-04-09 10:43:57 +0530

    Added ApiResponse Class and ApiError Classs

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,21 @@
+const wrapAsync = (fn) => {
+    return function (req, res, next) {
+      fn(req, res, next).catch((err) => next(err));
+    };
+  };
+
+
+export {wrapAsync}
+
+
+
+// const asyncHandler = (fn) => async (req, res, next)=>{
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message : error.message
+//         })
+//     }
+// }
